3cc6926c5189186666849468cc35bfaa22bab2d9
Alec Graham <alec.p.graham@protonmail.com>
2025-05-18 02:59:30 -0400

    error stuff

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 
+use std::fs;
 
 
-
-
-
-
+ 
 fn main() {
-    println!("Hello, world!");
+
+   match fs::read_to_string("logs.txt") {
+      Ok(text) =>{
+         println!("{}", text);
+         }
+      Err(err) =>{
+         println!("failed to find: {}", err);
+      }
+   }
+
+   
 }
