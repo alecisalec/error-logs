876ffe2c14bf9053bf8b87687b0480f59a49e621
Alec Graham <alec.p.graham@protonmail.com>
2025-05-19 08:54:03 -0400

    file stuff

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,48 @@
 
-use std::fs;
+use std::{fs};
 
 
+
+
+fn extract_errors(log: &str) -> Vec<String> {
+      let vec_strings=log.split("\n");
+      let mut ret_vec: Vec<String> = vec![];
+      for i in vec_strings {
+         if i.starts_with("ERROR"){
+            println!("{}", i);
+            ret_vec.push(i.to_string());
+         }
+         else{
+            println!("didnt match");
+         }
+         
+      }
+      return ret_vec;
+   }
  
 fn main() {
 
-   match fs::read_to_string("logs.txt") {
-      Ok(text) =>{
-         println!("{}", text);
-         }
-      Err(err) =>{
-         println!("failed to find: {}", err);
-      }
-   }
 
+   let text = fs::read_to_string("logs.txt").expect("failed to read file");
+
+   let error_logs = extract_errors(&text.as_str());
+
+   fs::write("errors.txt", error_logs.join("\n")).expect("there was an error");
    
+   // let mut err_logs = vec![];
+
+   // match fs::read_to_string("logs.txt") {
+   //    Ok(text) =>{
+   //       err_logs= extract_errors(text.as_str());
+         
+   //       }
+   //    Err(err) =>{
+   //       println!("failed to find: {}", err);
+   //    }
+   // }
+   // println!("{:#?}", err_logs);
+   
+
+
+
 }
